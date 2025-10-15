629921b340893ed71a7b56b45118a134b238735d
aravestg <75281165+aravestg@users.noreply.github.com>
2025-10-15 14:28:41 -0300

    Feature/223344-prueba-03 (#12)
    * Pruebas
    
    * Update db-stg-package-on-merge.yml
    
    * Update 01_FINANZAS_create_tabla_clientes.sql
    
    ---------
    
    Co-authored-by: Africa Ravest Gonzalez <Africa.RavestGonzalez@abcdin.cl>

--- db/migrations/223344-nueva-tabla-core/install/01_FINANZAS_create_tabla_clientes.sql
+++ db/migrations/223344-nueva-tabla-core/install/01_FINANZAS_create_tabla_clientes.sql
@@ -1,8 +1,8 @@
-BEGIN WORK;
+
 CREATE TABLE FINANZAS:tabla_clientes (
   id_cliente SERIAL PRIMARY KEY,
   rut        CHAR(12) NOT NULL,
   nombre     VARCHAR(120) NOT NULL,
   fecha_alta DATETIME YEAR TO DAY NOT NULL
 );
-COMMIT WORK;
+
