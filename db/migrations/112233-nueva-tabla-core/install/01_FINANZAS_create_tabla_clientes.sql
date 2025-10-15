9ee7e01f1f9a440cb8dbaa4c2017fc68c2043838
Africa Ravest Gonzalez <Africa.RavestGonzalez@abcdin.cl>
2025-10-15 13:59:46 -0300

    112233 nuevos cambios

---
+++ db/migrations/112233-nueva-tabla-core/install/01_FINANZAS_create_tabla_clientes.sql
@@ -0,0 +1,6 @@
+CREATE TABLE FINANZAS:tabla_clientes (
+  id_cliente SERIAL PRIMARY KEY,
+  rut        CHAR(12) NOT NULL,
+  nombre     VARCHAR(120) NOT NULL,
+  fecha_alta DATETIME YEAR TO DAY NOT NULL
+);
